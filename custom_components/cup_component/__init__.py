f771b696516bc543a7ec1312cf2a6ad1e77126a3
Bastien Gautier <contact@bastiengautier.com>
2025-05-05 18:09:44 +0000

    Clean code

--- custom_components/cup_component/__init__.py
+++ custom_components/cup_component/__init__.py
@@ -9,15 +9,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_NAME,
     CONF_URL,
-    # EVENT_HOMEASSISTANT_STOP,
     Platform,
 )
-
-# from homeassistant.core import Event, HomeAssistant, callback
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
-
-# from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -50,49 +45,12 @@ async def async_setup_entry(
 
     _LOGGER.debug("Setting up %s integration with host %s", DOMAIN, url)
 
-    # name_to_key = {
-    #     "Core Update Available": "core_update_available",
-    #     "Web Update Available": "web_update_available",
-    #     "FTL Update Available": "ftl_update_available",
-    #     "Status": "status",
-    #     "Ads Blocked Today": "ads_blocked_today",
-    #     "Ads Percentage Blocked Today": "ads_percentage_blocked_today",
-    #     "Seen Clients": "seen_clients",
-    #     "DNS Queries Today": "dns_queries_today",
-    #     "Domains Blocked": "domains_blocked",
-    #     "DNS Queries Cached": "dns_queries_cached",
-    #     "DNS Queries Forwarded": "dns_queries_forwarded",
-    #     "DNS Unique Clients": "dns_unique_clients",
-    #     "DNS Unique Domains": "dns_unique_domains",
-    #     "Remaining until blocking mode": "remaining_until_blocking_mode",
-    # }
-
-    # @callback
-    # def update_unique_id(
-    #     entity_entry: er.RegistryEntry,
-    # ) -> dict[str, str] | None:
-    #     """Update unique ID of entity entry."""
-    #     unique_id_parts = entity_entry.unique_id.split("/")
-    #     if len(unique_id_parts) == 2 and unique_id_parts[1] in name_to_key:
-    #         name = unique_id_parts[1]
-    #         new_unique_id = entity_entry.unique_id.replace(name, name_to_key[name])
-    #         _LOGGER.debug("Migrate %s to %s", entity_entry.unique_id, new_unique_id)
-    #         return {"new_unique_id": new_unique_id}
-    #
-    #     return None
-
-    # await er.async_migrate_entries(hass, entry.entry_id, update_unique_id)
-
     session = async_get_clientsession(hass, False)
     api_client = CupAPI(
         session=session,
         url=url,
         logger=_LOGGER,
     )
-
-    # entry.async_on_unload(
-    #     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_logout)
-    # )
 
     async def async_update_data() -> None:
         """Fetch data from API endpoint."""
